269525bd4c83a82a6451fc5e4896acbc2de32333
Sebastian Rettig <serettig@posteo.de>
2019-11-17 18:08:46 +0100

    improved logging

--- src/creator.js
+++ src/creator.js
@@ -1,31 +1,44 @@
-var scopackager = require('simple-scorm-packager');
-var fs = require('fs-extra');
+var scopackager = require("simple-scorm-packager");
+var fs = require("fs-extra");
 
 const templateDir = "./src/template";
 
-var cleanAndTrim = (text) => {
+var cleanAndTrim = text => {
   var textClean = text.replace(/[^a-zA-Z\d\s]/g, "");
   return textClean.replace(/\s/g, "");
-}
+};
 
 var creator = async (outputDir, h5pContentDir, tempDir, masteryScore) => {
+  const oldConsoleLog = console.log;
+  const log = "";
+  console.log = message => `${log}${message}\n`;
+
   if (!masteryScore) {
     masteryScore = 100;
   }
   await fs.remove(tempDir);
-  await fs.copy(templateDir, tempDir, { errorOnExist: false, overwrite: true, recursive: true });
-  await fs.copy(h5pContentDir, tempDir + "/workspace", { errorOnExist: false, overwrite: true, recursive: true })
+  await fs.copy(templateDir, tempDir, {
+    errorOnExist: false,
+    overwrite: true,
+    recursive: true
+  });
+  await fs.copy(h5pContentDir, tempDir + "/workspace", {
+    errorOnExist: false,
+    overwrite: true,
+    recursive: true
+  });
   try {
     const h5p = await fs.readJSON(h5pContentDir + "/h5p.json");
-    return new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) => {
       const options = {
-        version: '1.2',
-        organization: h5p.authors && h5p.authors[0] ? h5p.authors[0].name : 'H5P Author',
+        version: "1.2",
+        organization:
+          h5p.authors && h5p.authors[0] ? h5p.authors[0].name : "H5P Author",
         title: h5p.title || "H5P Content",
-        language: 'en-EN',
-        identifier: '00',
+        language: "en-EN",
+        identifier: "00",
         masteryScore: masteryScore,
-        startingPage: 'index.html',
+        startingPage: "index.html",
         source: tempDir,
         package: {
           version: "1.0.0",
@@ -36,17 +49,21 @@ var creator = async (outputDir, h5pContentDir, tempDir, masteryScore) => {
       };
       scopackager(options, async () => {
         await fs.remove(tempDir);
-        resolve(cleanAndTrim(options.title) +
-          "_v" +
-          options.package.version +
-          "_" +
-          options.package.date +
-          ".zip");
+        resolve(
+          cleanAndTrim(options.title) +
+            "_v" +
+            options.package.version +
+            "_" +
+            options.package.date +
+            ".zip"
+        );
       });
     });
   } catch (e) {
     await fs.remove(tempDir);
-    return undefined;
+    throw new Error(`Error when packaging SCORM:\n${log}`);
+  } finally {
+    console.log = oldConsoleLog;
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-var express = require('express');
-var fileupload = require('express-fileupload');
-var fs = require('fs-extra');
-var decompress = require('decompress');
-var creator = require('./creator').default;
-var ON_DEATH = require('death')({ uncaughtException: true });
-var _ = require('lodash');
-var fsExtra = require('fs-extra');
-var path = require('path');
+const _ = require("lodash");
+const chalk = require("chalk");
+const creator = require("./creator").default;
+const CsvWriter = require("csv-writer");
+const decompress = require("decompress");
+const express = require("express");
+const filesize = require("filesize");
+const fileupload = require("express-fileupload");
+const fs = require("fs-extra");
+const fsExtra = require("fs-extra");
+const ON_DEATH = require("death")({ uncaughtException: true });
+const path = require("path");
 
 const port = process.env.PORT || 80;
+const useStatistics = process.env.USE_STATISTICS
+  ? process.env.USE_STATISTICS.toLowerCase() === "true"
+  : false;
+const statisticsFile = path.resolve(
+  process.env.STATISTICS_FILE || "statistics.csv"
+);
 
 const workingDir = "working_directory";
 const uploadTmpDir = workingDir + "/downloads_tmp";
@@ -16,20 +25,20 @@ const h5pContentBaseDir = workingDir + "/workspace";
 const tempBaseDir = workingDir + "/temp";
 const outputDir = workingDir + "/output";
 
-var createDirectoryIfNecessary = async (dir) => {
+const createDirectoryIfNecessary = async dir => {
   if (!(await fs.exists(dir))) {
     await fs.mkdir(dir);
   }
-}
+};
 
-var createWorkingDirectories = async () => {
+const createWorkingDirectories = async () => {
   await createDirectoryIfNecessary(workingDir);
   await createDirectoryIfNecessary(uploadTmpDir);
   await createDirectoryIfNecessary(h5pContentBaseDir);
   await createDirectoryIfNecessary(outputDir);
-}
+};
 
-ON_DEATH(function (signal, err) {
+ON_DEATH(function(signal, err) {
   fs.removeSync(workingDir);
   process.exit();
 });
@@ -37,54 +46,134 @@ ON_DEATH(function (signal, err) {
 (async () => {
   await fs.remove(workingDir);
   await createWorkingDirectories();
-  var app = express();
+  const app = express();
   app.use(fileupload());
   app.use(express.urlencoded({ extended: false }));
 
-  // compile index.html
-  const compiled = _.template(await fsExtra.readFile(path.resolve('src/static/index.html')));
+  // compile index.html (to include site-specific files)
+  const compiled = _.template(
+    await fsExtra.readFile(path.resolve("src/static/index.html"))
+  );
   const index = compiled({
-    imprint: await fsExtra.readFile(path.resolve('static/imprint.html')),
-    privacy: await fsExtra.readFile(path.resolve('static/privacy.html')),
-    license: await fsExtra.readFile(path.resolve('static/license.html'))
+    imprint: await fsExtra.readFile(path.resolve("static/imprint.html")),
+    privacy: await fsExtra.readFile(path.resolve("static/privacy.html")),
+    license: await fsExtra.readFile(path.resolve("static/license.html"))
   });
-  app.get('/', (req, res) => {
+
+  // Express middleware
+  app.get("/", (req, res) => {
     res.send(index);
-  })
+  });
 
-  app.use(express.static('src/static'));
+  app.use(express.static("src/static"));
 
-  app.post('/convert', async function (req, res) {
-    var uploadedFile;
+  app.post("/convert", async function(req, res) {
+    let uploadedFile;
     if (!req.files || !req.files.h5p_file) {
-      return res.status(400).send();
+      return res.status(400).send("You must upload a H5P file.");
     }
     uploadedFile = req.files.h5p_file;
-
-    var masteryScore = req.body.h5p_mastery_score;
+    const masteryScore = req.body.h5p_mastery_score;
 
     const uploadedFilePath = uploadTmpDir + "/" + uploadedFile.name;
     const tempDir = tempBaseDir + "/" + uploadedFile.name;
-    uploadedFile.mv(uploadedFilePath, async (err) => {
+    uploadedFile.mv(uploadedFilePath, async err => {
       if (err) {
         return res.status(500).send(err);
       }
 
+      // create a working directory for the unpackaged H5P files
       const workspaceName = h5pContentBaseDir + "/" + uploadedFile.name;
       await decompress(uploadedFilePath, workspaceName);
       await fs.remove(uploadedFilePath);
-      const filename = await creator(outputDir, workspaceName, tempDir, masteryScore);
-      await fs.remove(workspaceName);
-      if (!filename) {
-        return res.status(400).send();
+
+      // read content type and version from H5P metadata
+      let contentType = "unknown";
+      let contentTypeMachineName = "unknown";
+      let contentTypeVersion = "unknown";
+      try {
+        const h5pMetadata = await fsExtra.readJSON(workspaceName + "/h5p.json");
+        const mainLibrary = h5pMetadata.preloadedDependencies.find(
+          dep => dep.machineName === h5pMetadata.mainLibrary
+        );
+        contentType = `${mainLibrary.machineName}-${mainLibrary.majorVersion}.${mainLibrary.minorVersion}`;
+        contentTypeMachineName = mainLibrary.machineName;
+        contentTypeVersion = `${mainLibrary.majorVersion}.${mainLibrary.minorVersion}`;
+      } catch (error) {
+        console.error(
+          chalk.red(
+            new Date().toLocaleString() +
+              " - Could not read main library of content."
+          )
+        );
       }
+
+      // package SCORM
+      let filename = "";
+      try {
+        filename = await creator(
+          outputDir,
+          workspaceName,
+          tempDir,
+          masteryScore
+        );
+      } catch (error) {
+        console.error(chalk.red(new Date().toLocaleString() + " - " + error));
+        filename = "";
+      } finally {
+        await fs.remove(workspaceName);
+      }
+      if (!filename) {
+        return res
+          .status(400)
+          .send(
+            "Something went wrong when creating the SCORM package. Try again!"
+          );
+      }
+
+      // Send created SCORM file to user and delete it
       res.download(outputDir + "/" + filename, async () => {
         await fs.remove(outputDir + "/" + filename);
       });
+
+      // Log success
+      console.log(
+        new Date().toLocaleString() +
+          ` - Successfully converted file (${contentType}, ${filesize(
+            req.files.h5p_file.size
+          )}).`
+      );
+
+      // Log usage for statistical purposes
+      if (useStatistics) {
+        const header = [
+          { id: "time", title: "Time" },
+          { id: "ctMachineName", title: "Content Type" },
+          { id: "ctVersion", title: "Content Type Version" },
+          { id: "filesize", title: "Size (in bytes)" }
+        ];
+        const csvWriter = CsvWriter.createObjectCsvWriter({
+          append: true,
+          path: statisticsFile,
+          header
+        });
+        if (!(await fsExtra.pathExists(statisticsFile))) {
+          const csvW = CsvWriter.createObjectCsvStringifier({ header });
+          await fsExtra.writeFile(statisticsFile, csvW.getHeaderString());
+        }
+        await csvWriter.writeRecords([
+          {
+            time: new Date().toUTCString(),
+            ctMachineName: contentTypeMachineName,
+            ctVersion: contentTypeVersion,
+            filesize: req.files.h5p_file.size
+          }
+        ]);
+      }
     });
   });
 
-  app.listen(port, function () {
-    console.log('listening on port ' + port);
+  app.listen(port, function() {
+    console.log(new Date().toLocaleString() + " - Listening on port " + port);
   });
 })();
